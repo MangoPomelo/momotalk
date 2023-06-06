01d52fb5bdc47cf20577f01c4088c0bc8652f244
MangoPomelo <3232426550@qq.com>
2023-06-06 09:23:57 +0800

    feat: use character dto in message list

--- src/components/MessageList/index.stories.js
+++ src/components/MessageList/index.stories.js
@@ -1,4 +1,5 @@
 import { MessageList } from '.';
+import { Character } from '../../class/character';
 import { Message } from '../Message';
 import React from 'react';
 
@@ -13,27 +14,23 @@ export default {
       options: ['Message1', 'Message2', 'Message3', 'Message4'],
       mapping: {
         Message1: <Message
-          avatar='/images/character/Sensei.png'
-          name='sensei'
+          character={new Character('sensei', '/images/character/Sensei.png')}
           content='Hello~!'
           primary
           fromMe
         />,
         Message2: <Message
-          avatar='/images/character/Sensei.png'
-          name='sensei'
+          character={new Character('sensei', '/images/character/Sensei.png')}
           content='How are you?'
           fromMe
         />,
         Message3: <Message
-          avatar='/images/character/Alice.png'
-          name='alice'
+          character={new Character('alice', '/images/character/Alice.png')}
           content='Fine, thanks!'
           primary
         />,
         Message4: <Message
-          avatar='/images/character/Alice.png'
-          name='alice'
+          character={new Character('alice', '/images/character/Alice.png')}
           content='How is it going?'
         />,
       },
