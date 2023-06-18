c7495da3db9f6e681a382340412875247b31ec29
MangoPomelo <3232426550@qq.com>
2023-06-19 01:46:51 +0800

    fix: wrong assets

--- src/pages/ChatPage/index.stories.js
+++ src/pages/ChatPage/index.stories.js
@@ -1,5 +1,6 @@
 import { ChatPage } from '.';
 import { CharacterData } from '../../classes/characterData';
+import { millennium } from '../../assets/schoolLogos';
 
 export default {
   title: 'Pages/ChatPage',
@@ -17,11 +18,10 @@ export const NoCandidate = {
 export const HasCandidates = {
   args: {
     candidates: [
-      new CharacterData('midori', '/images/character/Midori.png', 'game development development', '/images/emblem/millennium_science_academy.png'),
-      new CharacterData('momoi', '/images/character/Momoi.png', 'game development development', '/images/emblem/millennium_science_academy.png'),
-      new CharacterData('yuzu', '/images/character/Yuzu.png', 'game development development', '/images/emblem/millennium_science_academy.png'),
-      new CharacterData('alice', '/images/character/Alice.png', 'game development development', '/images/emblem/millennium_science_academy.png'),
-      new CharacterData('sensei', '/images/character/Sensei.png', '', '/images/emblem/dummy.png'),
+      new CharacterData('midori', 'https://schale.gg/images/student/collection/Student_Portrait_Midori_Collection.webp', 'game development development', millennium),
+      new CharacterData('momoi', 'https://schale.gg/images/student/collection/Student_Portrait_Momoi_Collection.webp', 'game development development', millennium),
+      new CharacterData('yuzu', 'https://schale.gg/images/student/collection/Student_Portrait_Yuzu_Collection.webp', 'game development development', millennium),
+      new CharacterData('alice', 'https://schale.gg/images/student/collection/Student_Portrait_Aris_Collection.webp', 'game development development', millennium),
     ],
   },
 };
